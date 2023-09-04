23b8d8c080073b9292bf480af3b498a2a1323985
LuccasVieira <118361303+LuccasVieira@users.noreply.github.com>
2023-09-04 19:46:38 -0300

    1

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -8,7 +8,7 @@ const PASS = process.env.DB_PASS;
 console.log(USER, PASS);
 
 export default mongoose.connect(
-  `mongodb+srv://${USER}:${PASS}@cluster0.vh3ibgl.mongodb.net/dripstore?retryWrites=true&w=majority`
+  `mongodb+srv://${USER}:${PASS}@cluster0.flrhk1g.mongodb.net/dripstore?retryWrites=true&w=majority`
 )
 .then(() => console.log('BD conectado com sucesso!'))
 .catch(error  => console.log(`Erro ao conectar ao banco: ${error}`))
